f56d502a7aeffaa5a18cb02e3431927460c872dd
ManaliGawade2023 <gawademanali2000@gmail.com>
2023-10-05 14:41:00 +0530

    first commit

---
+++ theme/footer.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>International world Conferences|Upcoming Business meetings and events - JSB Conference</title>
+
+      <!-- Favicon
+================================================== -->
+  <link rel="icon" type="image/png" href="images/conference-logo.png">
+
+<!-- CSS
+================================================== -->
+<!-- Bootstrap -->
+<link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<!-- FontAwesome -->
+<link rel="stylesheet" href="plugins/fontawesome/css/all.min.css">
+<!-- Animation -->
+<link rel="stylesheet" href="plugins/animate-css/animate.css">
+<!-- slick Carousel -->
+<link rel="stylesheet" href="plugins/slick/slick.css">
+<link rel="stylesheet" href="plugins/slick/slick-theme.css">
+<!-- Colorbox -->
+<link rel="stylesheet" href="plugins/colorbox/colorbox.css">
+<!-- Template styles-->
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/customized.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"/>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+    
+<footer id="footer" class="footer  footer-layer">
+    <div class="footer-main">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col-lg-3 col-md-6 footer-widget footer-about">
+            <h3 class="widget-title">Information Link</h3>
+            <img loading="lazy" width="200px" class="footer-logo" src="images/conference-logo.png" alt="JSB">
+            <div class="footer-menu">
+              <ul class="footer-link">
+                <li><a href="index.html" >Home</a></li>
+                <li><a href="about.html">About Us</a></li>
+                <li><a href="">Privacy Policy</a></li>
+              </ul>
+            </div><!-- Footer  -->
+          </div><!-- Col end -->
+
+
+          <div class="col-6 col-lg-3 col-md-6 mt-lg-0 footer-widget">
+            <h3 class="widget-title">Conference</h3>
+            <ul class="list-arrow">
+              <li><a href="">Previous Conference</a></li>
+              <li><a href="">Current Conference</a></li>
+              <li><a href="">Upcoming Conference</a></li>
+              </ul>
+          </div><!-- Col end -->
+
+
+          <div class="col-6 col-lg-3 col-md-6 footer-widget  mt-md-0">
+            <h3 class="widget-title">Follow Us</h3>
+            <div class="footer-social">
+              <ul class="">
+                <li><a href="https://www.facebook.com/JSBConference/" target="_blank" ><i class="bi bi-facebook"></i></a></li>
+                <li><a href="https://twitter.com/JsbConference" target="_blank"><i class="bi bi-twitter"></i></a></li>
+                <li><a href="https://www.youtube.com/channel/UCa9mHbn0iEgCvItvFSzwxIg?view_as=subscriber" target="_blank"><i class="bi bi-youtube"></i></a></li>
+              </ul>
+            </div><!-- Footer  -->
+          </div><!-- Col end -->  
+
+          <div class="col-lg-3 col-md-6  mt-lg-0 footer-widget">
+            <h3 class="widget-title">We Accept</h3>
+            <p>Pay with debit or credit card</p>
+            <div class="transaction">
+            <ul class="online" >
+              <li><a href=""><img src="images/mc.jpg"></a></li>
+              <li><a href=""><img src="images/mx.jpg"></a></li>
+              <li><a href=""><img src="images/visa.jpg"></a></li>
+              <li><a href=""><img src="images/pay-pal.jpg"></a></li>
+
+              </ul>
+            </div>
+          </div><!-- Col end -->
+
+        </div><!-- Row end -->
+      </div><!-- Container end -->
+    </div><!-- Footer main end -->
+
+    <div class="copyright">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12">
+            <div class="copyright-info text-center">
+              <span>Copyright &copy; <script>
+                  document.write(new Date().getFullYear())
+                </script>, Designed &amp; Developed by <a href="https://themefisher.com">JSB</a></span>
+            </div>
+          </div>
+
+
+        </div><!-- Row end -->
+
+        <div id="back-to-top" data-spy="affix" data-offset-top="10" class="back-to-top position-fixed">
+          <button class="btn btn-primary" title="Back to Top">
+            <i class="fa fa-angle-double-up"></i>
+          </button>
+        </div>
+
+      </div><!-- Container end -->
+    </div><!-- Copyright end -->
+  </footer><!-- Footer end -->
+
+</div><!-- Body inner end -->
+<script>
+  document.getElementById('showForm').addEventListener('click', function() {
+	var form = document.getElementById('myForm');
+	if (form.style.display === 'none') {
+	  form.style.display = 'block';
+	} else {
+	  form.style.display = 'none';
+	}
+  });
+</script>
+  <!-- Javascript Files
+  ================================================== -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <!-- initialize jQuery Library -->
+  <script src="plugins/jQuery/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <!-- Bootstrap jQuery -->
+  <script src="plugins/bootstrap/bootstrap.min.js" defer></script>
+  <!-- Slick Carousel -->
+  <script src="plugins/slick/slick.min.js"></script>
+  <script src="plugins/slick/slick-animation.min.js"></script>
+  <!-- Color box -->
+  <script src="plugins/colorbox/jquery.colorbox.js"></script>
+  <!-- shuffle -->
+  <script src="plugins/shuffle/shuffle.min.js" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  <!-- Google Map API Key-->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
+  <!-- Google Map Plugin-->
+  <script src="plugins/google-map/map.js" defer></script>
+
+  <!-- Template custom -->
+  <script src="js/script.js"></script>
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+
+  </body>
+</html>
---
+++ theme/header.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>International world Conferences|Upcoming Business meetings and events - JSB Conference</title>
+      <!-- Favicon
+================================================== -->
+  <link rel="icon" type="image/png" href="images/conference-logo.png">
+
+<!-- CSS
+================================================== -->
+<!-- Bootstrap -->
+<link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<!-- FontAwesome -->
+<link rel="stylesheet" href="plugins/fontawesome/css/all.min.css">
+<!-- Animation -->
+<link rel="stylesheet" href="plugins/animate-css/animate.css">
+<!-- slick Carousel -->
+<link rel="stylesheet" href="plugins/slick/slick.css">
+<link rel="stylesheet" href="plugins/slick/slick-theme.css">
+<!-- Colorbox -->
+<link rel="stylesheet" href="plugins/colorbox/colorbox.css">
+<!-- Template styles-->
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/customized.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"/>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+    #myForm {
+    display: none;
+    z-index: 100;
+   position: absolute;
+   top: 100% !important;
+   background-color: white;
+   color: #fff;
+   padding: 30px 30px;
+   border: 2px solid gray;
+  }
+  .login_data h2{
+    text-align: center;
+    font-size: 20px;
+    font-family: Montserrat;
+    font-weight: 700;
+    color: #074681;
+    text-transform: none !important;
+    padding:5px 0px;
+    padding-bottom: 10px;
+  }
+  .login label,.signUp{
+    color: black;
+    font-size: 16px;
+    margin: 10px 3px;
+  }
+  .signUp{
+    font-size: 14px;
+    text-align: center !important;
+
+  }
+.for a{
+text-align: right !important;
+float: right !important;
+font-size: 13px;
+}
+#login-Button{
+display: flex !important;
+justify-content: center !important;
+align-items: center;
+padding: 0.1rem 0.9rem;
+border: none !important;
+background-color: #074681;
+color: white;
+margin: 16% 37%;
+font-weight: 700;
+font-size: 16px;
+}
+.h-line{
+width: 100%;
+height: 1rem;
+background-color: gray;
+margin-top: 0.5px!important;
+}
+.element{
+text-align: center!important;
+color: #000;
+font-size: 16px;
+font-weight: 700;
+display: flex;
+justify-content: center;
+margin-top: -0.54rem;
+}
+#T{
+color: orange !important;
+font-size: 18px;
+margin: 0.3rem 0.3rem !important;
+
+}
+#F{
+color: rgb(10, 183, 252);
+font-size: 18px;
+margin: 0.3rem 0.3rem !important;
+
+}
+#S{
+color: rgb(10, 183, 252) ;
+font-size: 18px;
+margin: 0.3rem 0.3rem !important;
+
+}
+.options{
+display: flex;
+justify-content:center;
+margin: 0.3rem 0.3rem !important;
+}
+</style> 
+
+</head>
+<body>
+<header id="header" class="header-one">
+  <div class="bg-white front-bg">
+    <div class="container">
+      <div class="logo-area">
+          <div class="row align-items-center">
+            <div class="logo col-6 col-lg-3 text-center text-lg-left mb-3 mb-md-5 mb-lg-0">
+                <a class="d-block" href="index.html">
+                  <img loading="lazy" src="images/conference-logo.png" alt="Constra" width="100%" height="100%" >
+                </a>
+            </div><!-- logo end -->
+  
+            <div class="col-6 col-lg-9 justify-content-center header-right">
+                <ul class="top-info-box d-flex justify-content-center">
+                  <li class="header-get-a-quote ">
+                    <a class="btn btn-primary l-btn" href="#"  id="showForm">Login</a>
+                  </li>
+                </ul><!-- Ul end -->
+                  <div class="contanier form-container">
+                <form id="myForm">
+                  <div class="login_data">
+                    <h2>Login</h2>
+                    <div class="login">
+                    <label id="userLabel">Username</label>
+                    <input class="usInput" type="text" placeholder="Type your username"><br>
+                    <label id="passLabel">Password</label>
+                    <input class="pwInput" type="password" placeholder="Type your password">
+               <p class="for"> <a href="forgetpass.html">Forget Password?</a></p>
+               
+                    </div>
+                    <button id="login-Button">LOGIN</button>
+                    <div class="or">
+                        <hr class="h-line">
+                        <strong class="element">Or</strong>
+                    </div>
+                    <div class="options">
+                   <i id="T" class="bi bi-google"></i>
+                    <i id="F" class="bi bi-facebook"></i>
+                    <i id="S" class="bi bi-twitter"></i>
+                    </div>
+                    <p class="signUp">Need an account? <a href="registerform.html">Register Here</a></p>
+                </div>
+              </form>
+            </div>
+            </div><!-- header right end -->
+          </div><!-- logo area end -->
+  
+      </div><!-- Row end -->
+    </div><!-- Container end -->
+  </div>
+
+  <div class="site-navigation  nav-site">
+    <div class="container">
+        <div class="row ">
+          <div class="col-lg-12">
+              <nav class="navbar navbar-expand-lg navbar-dark p-0">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div id="navbar-collapse" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav mr-auto">
+                      <li class="nav-item dropdown active">
+                          <a href="#" class="nav-link">Home</a>
+                          <!-- <ul class="dropdown-menu" role="menu">
+                            <li class="active"><a href="index.html">Home One</a></li>
+                            <li><a href="index-2.html">Home Two</a></li>
+                          </ul> -->
+                      </li>
+
+                      <li class="nav-item dropdown">
+                          <a href="about.html" class="nav-link" >About</a>
+                          <!-- <ul class="dropdown-menu" role="menu">
+                            <li><a href="about.html">About Us</a></li>
+                            <li><a href="team.html">Our People</a></li>
+                            <li><a href="testimonials.html">Testimonials</a></li>
+                            <li><a href="faq.html">Faq</a></li>
+                            <li><a href="pricing.html">Pricing</a></li>
+                          </ul> -->
+                      </li>
+              
+                      <li class="nav-item dropdown">
+                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Conference<i class="fa fa-angle-down"></i></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="Current Conference.html">Current Conference</a></li>
+                            <li><a href="Previous Conference.html">Previous Conference</a></li>
+                            <li><a href="upcoming Conference.html">Upcoming Conference</a></li>
+
+                          </ul>
+                      </li>
+              
+                      <li class="nav-item dropdown">
+                        <a href="blog.html" class="nav-link" >Blogs</a>
+                        <!-- <ul class="dropdown-menu" role="menu">
+                          <li><a href="news-left-sidebar.html">News Left Sidebar</a></li>
+                          <li><a href="news-right-sidebar.html">News Right Sidebar</a></li>
+                          <li><a href="news-single.html">News Single</a></li>
+                        </ul> -->
+                    </li>
+                      <li class="nav-item dropdown">
+                          <a href="Reviews.html" class="nav-link" >Reviews</a>
+                          <!-- <ul class="dropdown-menu" role="menu">
+                            <li><a href="services.html">Services All</a></li>
+                            <li><a href="service-single.html">Services Single</a></li>
+                          </ul> -->
+                      </li>
+              
+                      <!-- <li class="nav-item dropdown">
+                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Features <i class="fa fa-angle-down"></i></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="typography.html">Typography</a></li>
+                            <li><a href="404.html">404</a></li>
+                            <li class="dropdown-submenu">
+                                <a href="#!" class="dropdown-toggle" data-toggle="dropdown">Parent Menu</a>
+                                <ul class="dropdown-menu">
+                                  <li><a href="#!">Child Menu 1</a></li>
+                                  <li><a href="#!">Child Menu 2</a></li>
+                                  <li><a href="#!">Child Menu 3</a></li>
+                                </ul>
+                            </li>
+                          </ul>
+                      </li> -->
+              
+                      <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+              </nav>
+          </div>
+          <!--/ Col end -->
+        </div>
+        <!--/ Row end -->
+
+        <div class="nav-search">
+          <span id="search"><i class="fa fa-search"></i></span>
+        </div><!-- Search end -->
+
+        <div class="search-block" style="display: none;">
+          <label for="search-field" class="w-100 mb-0">
+            <input type="text" class="form-control" id="search-field" placeholder="Type what you want and enter">
+          </label>
+          <span class="search-close">&times;</span>
+        </div><!-- Site search end -->
+    </div>
+    <!--/ Container end -->
+
+  </div>
+  <!--/ Navigation end -->
+</header>
+<!--/ Header end -->
+
+</body>
+</html>
